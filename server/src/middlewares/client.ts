a57b8b52b8ff25a6a928ac367f4e2ff7cb752758
mmfiber <masa.03191996@gmail.com>
2021-06-19 22:45:56 +0900

    refactor

--- server/src/middlewares/client.ts
+++ server/src/middlewares/client.ts
@@ -1,21 +1,17 @@
 import { Request, Response, NextFunction } from "express"
-import { ClientCredentials } from "../types/interfaces"
 
 export function parseClientCredentials(req: Request, _: Response, next: NextFunction ) {
+  req.clientCredentials = {}
   const auth = req.headers["authorization"]
   if(!auth) return next()
 
-  let credentials: ClientCredentials = {}
   const [type, code] = auth.split(" ")
   if(type.toLowerCase() === "basic") {
-    const decoded = decodeClientCredentials(auth)
-    credentials = decoded
+    req.clientCredentials = decodeClientCredentials(auth)
   }
   if(type.toLowerCase() === "bearer") {
-    credentials = { accessToken: code }
+    req.clientCredentials = { accessToken: code }
   }
-
-  req.clientCredentials = credentials
   next()
 }
 
