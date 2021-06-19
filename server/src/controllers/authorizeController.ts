363f63f696cbd39945a6f7bf053ecdeedd7fbf23
mmfiber <masa.03191996@gmail.com>
2021-06-19 20:42:58 +0900

    authorize controller

--- server/src/controllers/authorizeController.ts
+++ server/src/controllers/authorizeController.ts
@@ -1,9 +1,11 @@
 
 import { Request, Response } from "express"
+import { urlBuilder } from "../utils"
 
 export default class AuthorizeController {
-  public index (req: Request, res: Response) {
-    console.log(req)
-    res.json({"message": "hello world"})
+  public index (_: Request, res: Response) {
+    const url = urlBuilder("http://localhost:9001/", { code: "code" })
+    // const url = urlBuilder(req.params.redirect_uri, { code: "code" })
+    res.redirect(url)
   }
 }
--- server/src/utils.ts
+++ server/src/utils.ts
@@ -1,28 +1,33 @@
-import fs from "fs"
+// import fs from "fs"
 
-type file = {
-  path: string
-  name: string
-  extension: string
-}
-export function readdirRecursively (dir: string, files: file[] = []) {
-  const dirents = fs.readdirSync(dir, { withFileTypes: true })
-  const dirs = []
-  for (const dirent of dirents) {
-    if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`)
-    if (dirent.isFile()) {
-      const splitted = dirent.name.split(".")
-      const name = splitted.length > 1
-        ? splitted.slice(0, splitted.length - 1).join(".")
-        : splitted[0]
-      const extension = splitted.length > 1
-        ? splitted[splitted.length - 1]
-        : ""
-      files.push({ path: `${dir}/${dirent.name}`, name, extension })
-    }
-  }
-  for (const d of dirs) {
-    files = readdirRecursively(d, files)
-  }
-  return files
+// type File = {
+//   path: string
+//   name: string
+//   extension: string
+// }
+// export function readdirRecursively (dir: string, files: File[] = []) {
+//   const dirents = fs.readdirSync(dir, { withFileTypes: true })
+//   const dirs = []
+//   for (const dirent of dirents) {
+//     if (dirent.isDirectory()) dirs.push(`${dir}/${dirent.name}`)
+//     if (dirent.isFile()) {
+//       const splitted = dirent.name.split(".")
+//       const name = splitted.length > 1
+//         ? splitted.slice(0, splitted.length - 1).join(".")
+//         : splitted[0]
+//       const extension = splitted.length > 1
+//         ? splitted[splitted.length - 1]
+//         : ""
+//       files.push({ path: `${dir}/${dirent.name}`, name, extension })
+//     }
+//   }
+//   for (const d of dirs) {
+//     files = readdirRecursively(d, files)
+//   }
+//   return files
+// }
+
+export function urlBuilder(url: string, queries: any = {}) {
+  const query = Object.keys(queries).map(k => k + "=" + queries[k]).join("&")
+  return query ? url + "?" + query : url
 }
